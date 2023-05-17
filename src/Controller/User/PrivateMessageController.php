0b55eef540f7fddbbc80eb0727b3ba7cfaf3d2d1
BluedyAishela_ <94249947+BluedyRimuru@users.noreply.github.com>
2023-05-18 00:36:33 +0200

    #9 Envoi des données PHP -> JavaScript, fonction qui permet d'afficher dans un tableau les données insérées, fonction permettant de rechercher un utilisateur en fonction de son pseudonyme + css du tableau (à revoir pour les besoins du site)

--- src/Controller/User/PrivateMessageController.php
+++ src/Controller/User/PrivateMessageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\User;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class PrivateMessageController extends AbstractController
 {
     #[Route('/message', name: 'app_user_private_message')]
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
-        return $this->render('user/private-message.html.twig', [
+        $idUser = $this->getUser()->getUserIdentifier();
+        $dataUser = $userRepository->findOneBy(["email" => $idUser]);
 
+        $users = $userRepository->findAll();
+
+        return $this->render('user/private-message.html.twig', [
+            'users' => $users,
         ]);
     }
 }
